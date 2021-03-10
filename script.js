9534f2e0dbda529a0d00c8e99198785484a019e0
velaztro <68763480+velaztro@users.noreply.github.com>
2021-03-10 00:40:04 -0800

    Design

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 
 // Variables
-let email = document.getElementById("i-email");
-let img = document.getElementById("error");
-let btn = document.getElementById("btn");
-let invalid = document.getElementById("alert");
-let input = document.getElementById("form");
+const input = document.getElementById("input");
+const img = document.getElementById("error");
+const btn = document.getElementById("btn");
+const invalid = document.getElementById("alert");
+const border = document.getElementById("form");
 
 // Disappeared error alerts by clicking input.
-let again = () => {
-    email.addEventListener('click', () => {
+const again = () => {
+    input.addEventListener('click', () => {
         img.style.display = "none";
         invalid.style.display = "none";
-        input.style.border = "1px solid #c9c9c9";
+        border.style.border = "1px solid #c9c9c9";
     });
 };
 
-// Validate email structure.
-let validate = () => {
+// Disappeared error alerts by enter input.
+const hide = () => {
+    input.addEventListener('input', () => {
+        img.style.display = "none";
+        invalid.style.display = "none";
+        border.style.border = "1px solid #c9c9c9";
+    });
+};
+
+// Validate input structure.
+const validate = () => {
   let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (email.value.match(mailformat)) {
+  if (input.value.match(mailformat)) {
     return (true);
   } else {
     img.style.display = "block";
     invalid.textContent = "Please provide a valid email"
     invalid.style.display = "block";
-    input.style.border = "1px solid hsl(0, 93%, 68%)";
+    border.style.border = "1px solid hsl(0, 93%, 68%)";
     again();
   return false;
   }
 }
 
 // Validate if input is empty.
-let empty = () => {
-    if (email.value === "") {
+const empty = () => {
+    if (input.value === "") {
         img.style.display = "block";
         invalid.textContent = "Empty"
         invalid.style.display = "block";
         again();
+        hide();
         return (false);
     } else {
         validate();
